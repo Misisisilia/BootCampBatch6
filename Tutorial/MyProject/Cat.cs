84c10cfe191a937975264b8b32ecf21c47d62ef2
misisisilia@gmail.com <misisisilia@gmail.com>
2023-10-10 02:49:38 +0700

    init

---
+++ Tutorial/MyProject/Cat.cs
@@ -0,0 +1,32 @@
+namespace Tutorial;
+
+public class Cat
+
+{
+	public string Name;
+	public string Colour;
+	public int Leg;
+	public bool IsTailLong;
+	public int Weight;
+	public int Height;
+	
+	public void Jump()
+	{
+		Console.WriteLine("Jump");
+	}
+	
+	public void Run()
+		{
+			Console.WriteLine("Run");
+		}
+		
+	public void Eat()
+	{
+		Console.WriteLine("Eat");
+	}
+	
+	public void Drop()	
+	{
+		Console.WriteLine("Drop");
+	}
+}
--- Tutorial/MyProject/Program.cs
+++ Tutorial/MyProject/Program.cs
@@ -1,2 +1,58 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hellohh, World!");
+﻿using Tutorial;
+class Program
+{
+	 static void Main (string[]args)
+	
+	{
+		Cat oyen = new Cat();
+		
+		oyen.Name = "Oyen";
+		oyen.Colour = "Orange";
+		oyen.Leg = 6;
+		oyen.IsTailLong = true;
+		oyen.Weight = 20;
+		oyen.Height = 20;
+		
+		Console.Write("Name : ");
+		Console.WriteLine (oyen.Name);
+		Console.Write("Colour : ");
+		Console.WriteLine (oyen.Colour);
+		Console.Write("Leg : ");
+		Console.WriteLine (oyen.Leg);
+		Console.Write("Is Tail Long : ");
+		Console.WriteLine (oyen.IsTailLong);
+		Console.Write("Weight : ");
+		Console.WriteLine (oyen.Weight);
+		Console.Write("Height : ");
+		Console.WriteLine (oyen.Height);
+		
+		oyen.Jump ();
+		
+		Console.WriteLine();
+		
+		Cat Bambang = new Cat ();
+		
+		Bambang.Name = "Bambang";
+		Bambang.Colour = "Black";
+		Bambang.Leg = 5;
+		Bambang.IsTailLong = false;
+		Bambang.Weight = 28;
+		Bambang.Height = 30;
+		
+		Console.Write("Name : ");
+		Console.WriteLine (Bambang.Name);
+		Console.Write("Colour : ");
+		Console.WriteLine (Bambang.Colour);
+		Console.Write("Leg : ");
+		Console.WriteLine (Bambang.Leg);
+		Console.Write("Is Tail Long : ");
+		Console.WriteLine (Bambang.IsTailLong);
+		Console.Write("Weight : ");
+		Console.WriteLine (Bambang.Weight);
+		Console.Write("Height : ");
+		Console.WriteLine (Bambang.Height);
+		
+		Bambang.Run();
+		Console.ReadLine();
+	}
+}
