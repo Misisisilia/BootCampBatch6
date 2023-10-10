d0b884aab80bff3348a86c2f7bf9525c0166f6b9
Misisisilia <misisisilia@gmail.com>
2023-10-10 15:47:05 +0700

    new22222

---
+++ Day02A/Car.cs
@@ -0,0 +1,27 @@
+using System.Xml.Linq;
+
+namespace Transportation;
+
+class Cars
+{
+public string brand;
+public string type;
+public int door;
+public Cars ()
+
+{
+	brand = "Honda";
+	type = "Karimun";
+	door = 4;
+}
+
+
+
+// public Cars (string brand, string type, int door)
+
+// {
+// 	this.brand = brand;
+// 	this.type = type;
+// 	this.door = door;
+// }
+}
---
+++ Day02A/Program.cs
@@ -0,0 +1,17 @@
+﻿using Transportation;
+class Program 
+{
+	static void Main() 
+	{
+		// Car toyotaSupra = new Car();
+		// Console.WriteLine(toyotaSupra.brand);
+		// Console.WriteLine(toyotaSupra.type);
+		
+		// toyotaSupra.brand = "honda";
+		// Console.WriteLine(toyotaSupra.brand);
+		// Console.WriteLine(toyotaSupra.type);
+
+		Car l300 = new Car("mitsubishi","l300",2);
+		Console.WriteLine(l300.brands);
+	}
+}
