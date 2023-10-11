2c171284d0a994c745c151ab6029ff7201fbc2b2
Misisisilia <misisisilia@gmail.com>
2023-10-11 15:29:37 +0700

    overloading

---
+++ Day03C/Calculator.cs
@@ -0,0 +1,18 @@
+class Calculator 
+ {
+	
+	public void Add(int a, int b) {
+		Console.WriteLine(a+b);
+	}
+	
+	public void Add (int a, int b, int c){
+		Console.WriteLine(a*b+c);
+	}
+
+	public string Add (string a, string b, string c)
+	{
+			return a+b;
+	}
+
+
+}
---
+++ Day03C/Program.cs
@@ -0,0 +1,11 @@
+﻿class Program
+
+{
+	static void Main()
+	{
+		Calculator C = new();
+		C.Add (9,10);
+		C.Add (10,10,10);
+		Console.WriteLine(C.Add("3","10", "11"));
+	}
+}
