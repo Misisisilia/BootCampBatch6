f3b3c57b80af9ac86946ab0de41b982bf05d7ee2
Misisisilia <misisisilia@gmail.com>
2023-10-19 16:50:12 +0700

    HariKamis19Ok

---
+++ Day07/EventHandler(String)/Program.cs
@@ -0,0 +1,32 @@
+﻿//EventHandler<string>
+class Program
+{
+	static void Main()
+	{
+	Publisher pub = new Publisher("Misi");
+	Subscriber sub = new Subscriber();
+	pub.eventHandler += sub.Notification;
+	pub.SendNotification();
+}
+class Publisher
+{
+	private string _name;
+	public EventHandler<string> eventHandler;
+	public Publisher(string name) { _name = name; }
+	public void SendNotification()
+	{
+		eventHandler?.Invoke(this, "Misi");
+	}
+	public override string ToString()
+	{
+		return _name;
+	}
+}
+class Subscriber
+{
+	public void Notification(object sender, string e)
+	{
+		Console.WriteLine($"Called by {e}");
+	}
+}
+}
---
+++ Day07/EventHandler(eventdata)/Program.cs
@@ -0,0 +1,43 @@
+﻿//EventHandler<EventData>
+class Program
+{
+	static void Main()
+	{
+	Publisher pub = new Publisher("Misi");
+	Subscriber sub = new Subscriber();
+	pub.eventHandler += sub.Notification;
+	pub.SendNotification();
+}
+class Publisher
+{
+	private string _name;
+	public EventHandler<EventData> eventHandler;
+	public Publisher(string name) { _name = name; }
+	public void SendNotification()
+	{
+		eventHandler?.Invoke(this, new EventData() 
+		{
+			name = "Misi",
+			age = 23
+		});
+	}
+	public override string ToString()
+	{
+		return _name;
+	}
+}
+class Subscriber
+{
+	public void Notification(object sender, EventData e)
+	{
+		Console.WriteLine($"Called by {e.name} {e.age}");
+	}
+}
+class EventData : EventArgs {
+	public string name;
+	public int age;
+	public void Printer(string message) {
+		Console.WriteLine(message);
+	}
+}
+}
