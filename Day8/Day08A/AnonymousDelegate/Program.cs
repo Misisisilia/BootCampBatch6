902dae76d781165ee881f275f91e408c54f3a199
Misisisilia <misisisilia@gmail.com>
2023-10-20 16:58:40 +0700

    HariKamis19sore

---
+++ Day8/Day08A/AnonymousDelegate/Program.cs
@@ -0,0 +1,24 @@
+﻿//Anonymous Delegate (Lambda Expression)
+class Program{
+static void Main()
+{
+	// (parameter) => (return);
+	var method = (int x, int y) =>
+	{
+		return x + y;
+	};
+	
+	var printer = (object x) => Console.WriteLine(x)();
+	var nggangapa2in = () => Console.WriteLine("diam");
+	
+	printer(30);
+	int result = method(3,4);
+	Console.WriteLine(result)
+}
+
+int Add(int x, int y) {
+	return x + y;
+}
+
+int Add2 (int x, int y) => x+y;
+}
