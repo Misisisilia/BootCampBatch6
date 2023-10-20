2dfaa3c6cc2824e564a58de053da29945ec227d0
Misisisilia <misisisilia@gmail.com>
2023-10-20 18:53:52 +0700

    new

---
+++ Day8/Day08A/EnumUsage/Program.cs
@@ -0,0 +1,49 @@
+﻿//Card
+	
+// class Card
+// 	{
+// 		private CardStatus status;
+// 	}
+// 	public enum CardStatus
+// 	{
+// 		OnDeck,
+// 		OnPlayer,
+// 		Removed
+// 	}
+// 	public enum GameStatus
+// 	{
+// 		Win,
+// 		Lose,
+// 		Draw
+// 	}
+	public enum DaysOfWeek
+	{
+		Monday,
+		Tuesday,
+		Wednesday,
+		Thursday,
+		Friday,
+		Saturday,
+		Sunday
+	}
+class Program
+{
+	static void Main()
+	{
+		if (DateTime.Now.DayOfWeek == DayOfWeek.Sunday)
+		{
+			Console.WriteLine("Today is Sunday.");
+		}
+		else
+		{
+			Console.WriteLine("Today is not Sunday.");
+			Console.WriteLine($"Today is {DateTime.Now.DayOfWeek}.");
+			Console.WriteLine($"Today is the {(int)DayOfWeek.Tuesday}nd day of the week.");
+		}
+		// Use the enum in a foreach loop
+		foreach (DaysOfWeek day in Enum.GetValues(typeof(DaysOfWeek)))
+		{
+			Console.WriteLine(day);
+		}
+	}
+}
