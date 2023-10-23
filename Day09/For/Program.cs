069ad637a988ea24affd78bccf27b4afea03d09f
Misisisilia <misisisilia@gmail.com>
2023-10-23 14:48:46 +0700

    day9

--- Day09/For/Program.cs
+++ Day09/For/Program.cs
@@ -3,7 +3,7 @@
 	
 static void Main() {
 {
-	for(int x = 1; x< 10 ; x++) 
+	for(int x = 0; x< 10 ; x++) 
 	{
 		Printer(x);
 	}
@@ -11,8 +11,8 @@ static void Main() {
 	int[] arrayInt = {1,2,3,4,5,6,7,8,9,10};
 	foreach(int x in arrayInt) 
 	{
-		Printer(x);
-		if(x == 3) break;
+		Printer(y);
+		if(x == 5) break;
 	}
 }
 
