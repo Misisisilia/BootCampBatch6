5a94da3d5025da13f183fb752f4c0668adab9deb
Misisisilia <misisisilia@gmail.com>
2023-11-02 14:52:33 +0700

    DAY 17

--- Day17/String/Program.cs
+++ Day17/String/Program.cs
@@ -3,11 +3,12 @@
 	static void Main() 
 	{
 		string result = String.Empty;
-		int iteration = 100000;
+		int iteration = 100;
 		for(int i = 0; i < iteration; i++) 
 		{
-			result += "Hello";
-			result += "Misi";
+			result += "Misi ";
+			result += "Cantik";
 		}
+		Console.WriteLine(result);
 	}
 }
