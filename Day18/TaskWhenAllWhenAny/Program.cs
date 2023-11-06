e313437728cd7b20440f86b96042fd4d7c6c46c4
Misisisilia <misisisilia@gmail.com>
2023-11-06 15:37:20 +0700

    6  Nov

--- Day18/TaskWhenAllWhenAny/Program.cs
+++ Day18/TaskWhenAllWhenAny/Program.cs
@@ -20,7 +20,7 @@ class Program
 	}
 	static async Task<int> MultiplyByTwoAsync(int number)
 	{
-		await Task.Delay(10000);
-		return number * 2;
+		await Task.Delay(1000);
+		return number * 3;
 	}
 }
--- Day18/TaskWhenAny/Program.cs
+++ Day18/TaskWhenAny/Program.cs
@@ -6,7 +6,7 @@ class Program
     static async Task Main()
     {
         Task task1 = Task.Run(async () => { await Task.Delay(1000); Console.WriteLine("Hello from Task 1!"); });
-        Task task2 = Task.Run(async () => { await Task.Delay(1500); Console.WriteLine("Hello from Task 2!"); });
+        Task task2 = Task.Run(async () => { await Task.Delay(500); Console.WriteLine("Hello from Task 2!"); });
         Task completedTask = await Task.WhenAny(task1, task2);
         Console.WriteLine("First completed task: " + (completedTask == task1 ? "Task 1" : "Task 2"));
     }
