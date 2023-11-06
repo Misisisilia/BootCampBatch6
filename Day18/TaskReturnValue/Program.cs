2b15c0bfa6c477a3300825c7bbc634d74d3c57c8
Misisisilia <misisisilia@gmail.com>
2023-11-06 14:14:05 +0700

    6  Nov

---
+++ Day18/TaskReturnValue/Program.cs
@@ -0,0 +1,19 @@
+﻿﻿using System.Threading.Tasks;
+
+class Program
+{
+	static void Main()
+	{
+		int x = 3;
+		int y = 4;
+		Task<int> task = Task.Run(()=>Add(x,y));
+
+		int result = task.Result;
+		Console.WriteLine($"Result: {result}");
+	}
+	
+	static int Add(int a, int b)
+	{
+		return a + b;
+	}
+}
