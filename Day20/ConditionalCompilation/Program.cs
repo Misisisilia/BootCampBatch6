913a1304498efada952369e3004a52a27a627eb1
Misisisilia <misisisilia@gmail.com>
2023-11-07 14:34:12 +0700

    DAY 20

---
+++ Day20/ConditionalCompilation/Program.cs
@@ -0,0 +1,19 @@
+﻿﻿//For choose between different Testing or Lib
+#define GAMETESTER
+#define GAMERUNNER
+
+class Program {
+	static void Main() {
+		#if GAMERUNNER
+		Console.WriteLine("GameRunner.");
+		//GameRunner gameRunner = new();
+		#elif GAMETESTER
+		Console.WriteLine("GameTester.");
+		//GameTester gameTest = new();
+		//methodTest(0);
+		#else
+		Console.WriteLine("Not anything.");
+		#endif
+		Console.WriteLine("Finish");
+	}
+}
