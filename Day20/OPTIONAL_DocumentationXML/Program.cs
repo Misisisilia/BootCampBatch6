d8e8d4f7b4cdf49ff4c50d458b3d2018df889541
Misisisilia <misisisilia@gmail.com>
2023-11-08 15:31:26 +0700

    DAY 21

--- Day20/OPTIONAL_DocumentationXML/Program.cs
+++ Day20/OPTIONAL_DocumentationXML/Program.cs
@@ -1,12 +1,13 @@
 ﻿﻿using DocumentationXML;
 static class Program
 {
+	
 	static void Main() 
 	{
 		double a = 4.5;
 		double b = 2.5;
-        double Add = MyLib.Add(a, b);
-        double Mul = MyLib.Mul(a, b);
+		double Add = MyLib.Add(a, b);
+		double Mul = MyLib.Mul(a, b);
 		double Div = MyLib.Div(a, b);
 		double Sub = MyLib.Sub(a, b);
 		string answer = $"{Add} {Mul} {Div} {Sub}";
