3581fc539ba2aba26edcf9989a2563d467db8b58
Misisisilia <misisisilia@gmail.com>
2023-11-23 13:34:47 +0700

    23nov

---
+++ Day30/WithDecorator/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+
+// Komponen dasar
+interface ICoffee
+{
+	void MakeCoffee();
+}
+
+// Komponen konkret
+class SimpleCoffee : ICoffee
+{
+	public void MakeCoffee()
+	{
+		Console.WriteLine("Membuat kopi dasar.");
+	}
+}
+
+// Dekorator abstrak
+abstract class CoffeeDecorator : ICoffee
+{
+	protected ICoffee coffee;
+
+	public CoffeeDecorator(ICoffee coffee)
+	{
+		this.coffee = coffee;
+	}
+
+	public virtual void MakeCoffee()
+	{
+		coffee.MakeCoffee();
+	}
+}
+
+// Dekorator konkret
+class MilkDecorator : CoffeeDecorator
+{
+	public MilkDecorator(ICoffee coffee) : base(coffee)
+	{
+	}
+
+	public override void MakeCoffee()
+	{
+		base.MakeCoffee();
+		AddMilk();
+	}
+
+	private void AddMilk()
+	{
+		Console.WriteLine("Tambahkan susu.");
+	}
+}
+
+// Dekorator konkret
+class SugarDecorator : CoffeeDecorator
+{
+	public SugarDecorator(ICoffee coffee) : base(coffee)
+	{
+	}
+
+	public override void MakeCoffee()
+	{
+		base.MakeCoffee();
+		AddSugar();
+	}
+
+	private void AddSugar()
+	{
+		Console.WriteLine("Tambahkan gula.");
+	}
+}
+
+// Penggunaan dengan decorator
+class Program
+{
+	static void Main()
+	{
+		// Penggunaan tanpa dekorator
+		ICoffee simpleCoffee = new SimpleCoffee();
+		simpleCoffee.MakeCoffee();
+
+		Console.WriteLine();
+
+		// Penggunaan dengan dekorator Milk
+		ICoffee milkCoffee = new MilkDecorator(new SimpleCoffee());
+		milkCoffee.MakeCoffee();
+
+		Console.WriteLine();
+
+		// Penggunaan dengan dekorator Sugar
+		ICoffee sugarCoffee = new SugarDecorator(new SimpleCoffee());
+		sugarCoffee.MakeCoffee();
+
+		Console.WriteLine();
+
+		// Penggunaan dengan dekorator Milk dan Sugar
+		ICoffee milkAndSugarCoffee = new SugarDecorator(new MilkDecorator(new SimpleCoffee()));
+		milkAndSugarCoffee.MakeCoffee();
+	}
+}
---
+++ Day30/WithoutDecorator/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+// Komponen dasar
+class SimpleCoffee
+{
+	public void MakeCoffee()
+	{
+		Console.WriteLine("Membuat kopi dasar.");
+	}
+}
+
+// Penggunaan tanpa decorator
+class Program
+{
+	static void Main()
+	{
+		SimpleCoffee coffee = new SimpleCoffee();
+		coffee.MakeCoffee();
+	}
+}
