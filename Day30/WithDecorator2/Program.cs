7ef89fd7dd67934243152574a595e0511e31ebea
Misisisilia <misisisilia@gmail.com>
2023-11-24 12:45:05 +0700

    24nov

---
+++ Day30/WithDecorator2/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+
+// Component Interface
+public abstract class Beverage
+{
+    public abstract string GetDescription();
+    public abstract double GetCost();
+}
+
+// Concrete Component
+public class Coffee : Beverage
+{
+    public override string GetDescription()
+    {
+        return "Coffee";
+    }
+
+    public override double GetCost()
+    {
+        return 2.0; // Harga dasar kopi
+    }
+}
+
+// Decorator
+public abstract class CondimentDecorator : Beverage
+{
+    protected Beverage _beverage;
+
+    public CondimentDecorator(Beverage beverage)
+    {
+        _beverage = beverage;
+    }
+
+    public override string GetDescription()
+    {
+        return _beverage.GetDescription();
+    }
+
+    public override double GetCost()
+    {
+        return _beverage.GetCost();
+    }
+}
+
+// Concrete Decorator
+public class SugarDecorator : CondimentDecorator
+{
+    public SugarDecorator(Beverage beverage) : base(beverage)
+    {
+    }
+
+    public override string GetDescription()
+    {
+        return $"{_beverage.GetDescription()}, Sugar";
+    }
+
+    public override double GetCost()
+    {
+        return _beverage.GetCost() + 0.5; // Harga gula
+    }
+}
+
+// Concrete Decorator
+public class MilkDecorator : CondimentDecorator
+{
+    public MilkDecorator(Beverage beverage) : base(beverage)
+    {
+    }
+
+    public override string GetDescription()
+    {
+        return $"{_beverage.GetDescription()}, Milk";
+    }
+
+    public override double GetCost()
+    {
+        return _beverage.GetCost() + 1.0; // Harga susu
+    }
+}
+
+// Concrete Decorator
+public class IceDecorator : CondimentDecorator
+{
+    public IceDecorator(Beverage beverage) : base(beverage)
+    {
+    }
+
+    public override string GetDescription()
+    {
+        return $"{_beverage.GetDescription()}, Ice";
+    }
+
+    public override double GetCost()
+    {
+        return _beverage.GetCost() + 0.7; // Harga es
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Pesan kopi tanpa tambahan
+        Beverage coffee = new Coffee();
+        Console.WriteLine($"Order: {coffee.GetDescription()}, Cost: ${coffee.GetCost()}");
+
+        // Pesan kopi dengan gula
+        Beverage sugarCoffee = new SugarDecorator(new Coffee());
+        Console.WriteLine($"Order: {sugarCoffee.GetDescription()}, Cost: ${sugarCoffee.GetCost()}");
+
+        // Pesan kopi dengan susu dan es
+        Beverage milkIceCoffee = new IceDecorator(new MilkDecorator(new Coffee()));
+        Console.WriteLine($"Order: {milkIceCoffee.GetDescription()}, Cost: ${milkIceCoffee.GetCost()}");
+    }
+}
---
+++ Day30/WithoutDecorator/Program.cs
@@ -0,0 +1,88 @@
+﻿public abstract class Product
+{
+    public abstract decimal GetPrice();
+    public virtual string GetDescription()
+    {
+        return "Basic Product";
+    }
+}
+
+public class BasicProduct : Product
+{
+    public override decimal GetPrice()
+    {
+        return 50.0m;
+    }
+}
+
+public class FastShippingProduct : Product
+{
+    private Product _product;
+
+    public FastShippingProduct(Product product)
+    {
+        _product = product;
+    }
+
+    public override decimal GetPrice()
+    {
+        return _product.GetPrice() + 10.0m;
+    }
+
+    public override string GetDescription()
+    {
+        return $"{_product.GetDescription()}, Fast Shipping";
+    }
+}
+
+public class InsuranceProduct : Product
+{
+    private Product _product;
+
+    public InsuranceProduct(Product product)
+    {
+        _product = product;
+    }
+
+    public override decimal GetPrice()
+    {
+        return _product.GetPrice() + 5.0m;
+    }
+
+    public override string GetDescription()
+    {
+        return $"{_product.GetDescription()}, Insurance";
+    }
+}
+
+public class ShoppingCart
+{
+    public void AddToCart(Product product)
+    {
+        Console.WriteLine($"Added to cart: {product.GetDescription()}, Price: ${product.GetPrice()}");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ShoppingCart cart = new ShoppingCart();
+
+        // Produk dasar
+        BasicProduct basicProduct = new BasicProduct();
+        cart.AddToCart(basicProduct);
+
+        // Produk dengan pengiriman cepat
+        FastShippingProduct fastShippingProduct = new FastShippingProduct(basicProduct);
+        cart.AddToCart(fastShippingProduct);
+
+        // Produk dengan asuransi
+        InsuranceProduct insuranceProduct = new InsuranceProduct(basicProduct);
+        cart.AddToCart(insuranceProduct);
+
+        // Produk dengan pengiriman cepat dan asuransi
+        InsuranceProduct insuranceFastShippingProduct = new InsuranceProduct(fastShippingProduct);
+        cart.AddToCart(insuranceFastShippingProduct);
+    }
+}
